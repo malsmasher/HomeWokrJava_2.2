737d86eec9f5ad98c1780ecdab4ee3e8490e926b
m.malyshonok <masha.malyshonok@gmail.com>
2024-05-28 20:27:06 +0500

    UPD Задача 2

--- src/Main.java
+++ src/Main.java
@@ -2,14 +2,14 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        int account_status = 50;
-        int refill_amount = 3000;
-        if (refill_amount > 1000) {
+        int accountStatus = 50;
+        int refillAmount = 3000;
+        if (refillAmount > 1000) {
             System.out.println("Итоговый счёт:");
-            System.out.println(account_status + refill_amount + refill_amount/100*1);
+            System.out.println(accountStatus + refillAmount + refillAmount/100*1);
 
             System.out.println("Начислено бонусов:");
-            System.out.println(refill_amount/100*1);
+            System.out.println(refillAmount/100*1);
         }
     }
 }
