8d7dc0d13d000a9fc60d7a4fb4074bfefe20f303
m.malyshonok <masha.malyshonok@gmail.com>
2024-05-28 17:31:39 +0500

    Задача 2

--- src/Main.java
+++ src/Main.java
@@ -2,3 +2,15 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
+        int x = 50;
+        int y = 3000;
+        if (y > 1000) {
+            System.out.println("Итоговый счёт:");
+            System.out.println(x + y + y/100*1);
+
+            System.out.println("Начислено бонусов:");
+            System.out.println(y/100*1);
+        }
+    }
+}
+    
